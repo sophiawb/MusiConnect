71ff62de9e9fc8b2b3a5fee9ce620eb7451a184d
Hack Reactor Students <accounts@hackreactor.com>
2015-08-07 14:35:28 -0700

    finish front end rough draft

--- client/services/services.js
+++ client/services/services.js
@@ -14,24 +14,68 @@ angular.module('app.services', [])
       templateUrl: 'nav/loggedoutnav.html'
     }
   }])
-  .factory('Signin', ['$http', '$location', function($http, $location){
+  .factory('HttpRequests', ['$http', '$location', function($http, $location){
     
-    // var signupUser = function(userObject){
-    //   // send to firebase
-    //   // var uid = UID from firebase
-    //   console.log('user http request sent');
-    //   $http.post('http://requestb.in/1h0kipc1', { // route will be '/api/users'
-    //     UID: uid,
-    //     talents: 
-    //   }).then(function(response){
-    //     console.log('user posted', response);
-    //     $location.path('/'); // TODO: where should this lead to?
-    //   }, function(err) {
-    //     console.log('error posting user', err);
-    //   }); 
-    // };
+    var signupUser = function(userObject){
+      // send to firebase
+      // var uid = UID from firebase
+      console.log('user http request sent', userObject);
+      return $http({
+        method: 'POST',
+        url: 'http://requestb.in/1h0kipc1',
+        data: userObject,
+        headers: {
+          'Content-Type': 'text/json'
+        }
+      }); // route will be '/api/users' 
+    };
+
+    var loginUser = function(){
+      // do authentication login here, promisified
+      // then redirect
+      $location.path('/user');
+    };
+
+    var getUser = function(uid) {
+      return $http({
+        method: 'GET',
+        url: 'http://requestb.in/1h0kipc1', // route will be '/api/users' + uid 
+      });
+      // .then( set username in localstorage?); 
+    };
+
+    var post    
+
+    var getRequests = function(uid) { // optional filter by username
+      var url;
+      if (username !== undefined) {
+        url = 'api/requests/' + uid;
+      } else {
+        url = 'api/requests/';
+      }
+      return $http({
+        method: 'GET',
+        url: url // route will be '/api/users' + uid 
+      });
+    };
+
+    var postRequest = function(request){
+      console.log('post request http request sent', request);
+      return $http({
+        method: 'POST',
+        url: 'http://requestb.in/1h0kipc1',
+        data: userObject,
+        headers: {
+          'Content-Type': 'text/json'
+        }
+      });
+    }
 
     return {
-      // signupUser: signupUser
+      loginUser: loginUser
+      signupUser: signupUser,
+      getUser: getUser,
+      getRequests: getRequests,
+      postRequest: postRequest
     }
   }]);
--- client/signin/signin.js
+++ client/signin/signin.js
@@ -1,24 +1,44 @@
 angular.module('app.signin', ['app.services'])
 
-.controller('signinController', ['$scope', 'Signin', function($scope, Signin){
+.controller('signinController', ['$scope', '$location', 'HttpRequests', function($scope, $location, Signin){
   $scope.user = {
     talents: []
   };
 
   $scope.newTalent = {};
 
+
   $scope.addTalent = function(){
     if ( $scope.newTalent.talent !== "" && $scope.newTalent.level !== ""){
       $scope.user.talents.push({
-      talent: $scope.newTalent.talent,
-      level: $scope.newTalent.level}
-    });
+        talent: $scope.newTalent.talent,
+        level: $scope.newTalent.level
+      });
     $scope.newTalent.talent = "";
     $scope.newTalent.level = "";
+    }
+    else {
+      // TODO: add a message that one of the forms is blank
+    }
   };
 
   $scope.signUp = function() {
-    // Signin.signupUser($scope.user);
+    $scope.user.talents = convertTalentsToObject()
+    console.log($scope.user);
+    HttpRequests.signupUser($scope.user) // signupUser returns a promise
+      .then(function(response){
+        console.log('user posted', response);
+        $location.path('/'); // TODO: where should this lead to?
+      }, function(err) {
+        console.log('error posting user', err);
+      }); 
   };
 
+  var convertTalentsToObject = function() {
+    var converted = {};
+    for (var i = 0; i < $scope.user.talents.length; i++){
+      converted[$scope.user.talents.talent] = $scope.user.talents.level; 
+    }
+    return converted;
+  };
 }]);
